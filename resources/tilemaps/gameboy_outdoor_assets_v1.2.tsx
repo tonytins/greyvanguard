5ae5c4793b1fe30a7191c414e9f26a3a0f8761fd
Tony Bark <35226681+tonytins@users.noreply.github.com>
2021-05-18 04:09:46 -0400

    Expanded Furtropolis' map
    - Added more NPCs to help flesh out lore
    - Ignore archives

---
+++ resources/tilemaps/gameboy_outdoor_assets_v1.2.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="gameboy_outdoor_assets_v1.2" tilewidth="8" tileheight="8" tilecount="840" columns="30">
+ <transformations hflip="1" vflip="1" rotate="1" preferuntransformed="0"/>
+ <image source="gameboy_outdoor_assets_v1.2.png" trans="dff9cd" width="240" height="224"/>
+</tileset>
---
+++ resources/tilemaps/hometileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="hometileset" tilewidth="8" tileheight="8" tilecount="768" columns="32">
+ <image source="hometileset.png" trans="dff9cd" width="256" height="192"/>
+</tileset>
